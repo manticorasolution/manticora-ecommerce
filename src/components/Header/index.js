a8f00937d74eede998a2af6cb0b6b716db5b9e86
Marcos Santana <brmarcossantanabr@gmail.com>
2020-10-11 15:00:15 -0300

    Create header component

---
+++ src/components/Header/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { AiOutlineSearch, AiOutlineShoppingCart } from 'react-icons/ai';
+import { BsPerson } from 'react-icons/bs';
+import {
+  Container, Menu, MenuItem, IconItem, Badge,
+} from './styles';
+
+import Manticora from '../../assets/manticora-black.png';
+import { useCart } from '../../contexts/cart';
+
+const Header = () => {
+  const { getQuantity } = useCart();
+
+  return (
+    <Container>
+      <Menu>
+        <MenuItem>
+          <Link to="/">
+            <img src={Manticora} alt="Manticora" height={75} />
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/">
+            <span>Home</span>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/">
+            <span>Produtos</span>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/">
+            <span>Sobre</span>
+          </Link>
+        </MenuItem>
+        <MenuItem>
+          <Link to="/">
+            <span>Contato</span>
+          </Link>
+        </MenuItem>
+
+      </Menu>
+
+      <Menu>
+        <IconItem>
+          <Link to="/">
+            <AiOutlineSearch size={20} />
+          </Link>
+        </IconItem>
+
+        <IconItem>
+          <Link to="/">
+            <BsPerson size={20} />
+          </Link>
+        </IconItem>
+
+        <IconItem>
+          <Link to="/cart">
+            <AiOutlineShoppingCart size={20} />
+            <Badge>{getQuantity()}</Badge>
+          </Link>
+        </IconItem>
+      </Menu>
+    </Container>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 50px 0;
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+export const MenuItem = styled.span`
+    margin: 0 10px;
+
+    a {
+        text-decoration: none;
+        text-transform: uppercase;
+        color: #000;
+        font-weight: bold;
+        font-size: 16px;
+    }
+`;
+
+export const IconItem = styled.div`
+  display: inline-block;
+    margin: 0 5px;
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+`;
+
+export const Badge = styled.div`
+  display: inline-block;
+  width: 20px;
+  line-height: 15px;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 50%;
+  color: #fff;
+  background: #f00;
+`;
